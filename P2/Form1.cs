4c2e45323127d6f503aad46bd21f7ce88e4759e3
Luiz Gustavo Arduino Batista <larduinobatista@gmail.com>
2025-06-11 17:34:04 -0300

    sistema de login feito e funcionando.

--- P2/Form1.cs
+++ P2/Form1.cs
@@ -5,16 +5,55 @@ namespace P2
         public Form1()
         {
             InitializeComponent();
+            inicia_arquivo();
 
         }
+        private string caminho_arquivo = "c:/Users/lardu/Documents/usuario.csv";
 
 
         private void btn_login_Click(object sender, EventArgs e)
         {
-            
+            string login = text_login.Text.Trim();
+            string senha = text_senha.Text.Trim();
+
+            if(string.IsNullOrWhiteSpace(login) || string.IsNullOrWhiteSpace(senha))
+            {
+                MessageBox.Show("Esta faltando dados");
+                return;
+            }
+
+            if (!File.Exists(caminho_arquivo))
+            {
+                MessageBox.Show("Banco de dados nao encontrado");
+                return;
+            }
+
+            string[] linhas = File.ReadAllLines(caminho_arquivo);
+
+            for (int i = 1; i < linhas.Length; i++)
+            {
+                string[] dados = linhas[i].Split(',');
+
+                if (dados.Length == 2)
+                {
+                    string logins = dados[0];
+                    string senhas = dados[1];
+
+                    if (logins.Equals(login, StringComparison.OrdinalIgnoreCase) && senhas == senha)
+                    {
+                        MessageBox.Show("Bem vindo");
+                        this.Hide();
+                        menu Menu = new menu();
+                        Menu.ShowDialog();
+                        this.Close();
+                        return;
+                    }
+                }
+            }
+
+            MessageBox.Show("Dados invalidos");
         }
 
-        private string caminho_arquivo = "c:/Users/lardu/Documents/usuario.csv";
 
         private void inicia_arquivo()
         {
