bc2df16b30e5313200380a7c3ee083cd16f6a9d6
Luiz Gustavo Arduino Batista <larduinobatista@gmail.com>
2025-06-11 16:30:10 -0300

    pagina de consultar pedidos criada

---
+++ P2/consultar_pedidos.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace P2
+{
+    partial class consultar_pedidos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            text_cpf = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            listView1 = new ListView();
+            list_itens = new ListView();
+            label_pedido = new Label();
+            label3 = new Label();
+            btn_buscar = new Button();
+            SuspendLayout();
+            // 
+            // text_cpf
+            // 
+            text_cpf.Location = new Point(33, 91);
+            text_cpf.Name = "text_cpf";
+            text_cpf.Size = new Size(209, 23);
+            text_cpf.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(33, 73);
+            label1.Name = "label1";
+            label1.Size = new Size(28, 15);
+            label1.TabIndex = 1;
+            label1.Text = "CPF";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(259, 19);
+            label2.Name = "label2";
+            label2.Size = new Size(122, 15);
+            label2.TabIndex = 2;
+            label2.Text = "CONSULTAR PEDIDOS";
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(33, 194);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(245, 119);
+            listView1.TabIndex = 3;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // list_itens
+            // 
+            list_itens.Location = new Point(376, 194);
+            list_itens.Name = "list_itens";
+            list_itens.Size = new Size(267, 119);
+            list_itens.TabIndex = 4;
+            list_itens.UseCompatibleStateImageBehavior = false;
+            // 
+            // label_pedido
+            // 
+            label_pedido.AutoSize = true;
+            label_pedido.Location = new Point(378, 172);
+            label_pedido.Name = "label_pedido";
+            label_pedido.Size = new Size(38, 15);
+            label_pedido.TabIndex = 5;
+            label_pedido.Text = "label3";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(33, 172);
+            label3.Name = "label3";
+            label3.Size = new Size(49, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Pedidos";
+            // 
+            // btn_buscar
+            // 
+            btn_buscar.Location = new Point(104, 120);
+            btn_buscar.Name = "btn_buscar";
+            btn_buscar.Size = new Size(75, 23);
+            btn_buscar.TabIndex = 7;
+            btn_buscar.Text = "Buscar";
+            btn_buscar.UseVisualStyleBackColor = true;
+            // 
+            // consultar_pedidos
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(688, 450);
+            Controls.Add(btn_buscar);
+            Controls.Add(label3);
+            Controls.Add(label_pedido);
+            Controls.Add(list_itens);
+            Controls.Add(listView1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(text_cpf);
+            Name = "consultar_pedidos";
+            Text = "consultar_pedidos";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox text_cpf;
+        private Label label1;
+        private Label label2;
+        private ListView listView1;
+        private ListView list_itens;
+        private Label label_pedido;
+        private Label label3;
+        private Button btn_buscar;
+    }
+}
--- P2/obj/Debug/net7.0-windows/P2.AssemblyInfo.cs
+++ P2/obj/Debug/net7.0-windows/P2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5dbee8715c0b5ca417e8ede9ea5db26702c9654")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fae5b88909ae8bfd23de7909c4db6b0251a42fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("P2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
