48640f7b8c4e619d108170d94e144e6b93228350
Luiz Gustavo Arduino Batista <larduinobatista@gmail.com>
2025-06-11 17:18:46 -0300

    fazendo sistema de inicar arquivo csv no painel de login

--- P2/Form1.cs
+++ P2/Form1.cs
@@ -5,15 +5,24 @@ namespace P2
         public Form1()
         {
             InitializeComponent();
-            
+
         }
 
 
         private void btn_login_Click(object sender, EventArgs e)
         {
-
+            
         }
 
+        private string caminho_arquivo = "c:/Users/lardu/Documents/usuario.csv";
+
+        private void inicia_arquivo()
+        {
+            using (StreamWriter aq = new StreamWriter(caminho_arquivo))
+            {
+                aq.WriteLine("Login,Senha");
+            }
+        }
 
     }
 }
