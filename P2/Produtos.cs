98f65b9ddb29d6f60bb1b4ef4f50f91b83462981
Luiz Gustavo Arduino Batista <larduinobatista@gmail.com>
2025-06-12 14:22:15 -0300

    sistema de salvar produto criado e funcionando.

--- P2/Produtos.cs
+++ P2/Produtos.cs
@@ -33,7 +33,34 @@ namespace P2
 
         private void btn_salvar_Click(object sender, EventArgs e)
         {
+            string nome = text_nome.Text.Trim();
+            string preco = text_preco.Text.Trim();
+            string descricao = text_descricao.Text.Trim();
 
+            if (string.IsNullOrEmpty(nome) ||
+                string.IsNullOrEmpty(preco) ||
+                string.IsNullOrEmpty(descricao))
+            {
+                MessageBox.Show("Esta faltando dados");
+            }
+
+            string linha = $"{nome},{preco},{descricao}";
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(caminho_arquivo, true))
+                {
+                    sw.WriteLine(linha);
+                }
+                MessageBox.Show("Produto criado", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                text_nome.Clear();
+                text_preco.Clear();
+                text_descricao.Clear();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Não foi possivel salvar o produto");
+            }
         }
     }
 }
